a6ac2f8a7fd3da6db15c48d1f12308155eaf8155
qianchenghui <qch11111@frsky-rc.com>
2021-04-08 19:08:13 +0800

    2021/4/8 P18

--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -3,10 +3,14 @@ package life.majiang.community.mapper;
 import life.majiang.community.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("INSERT into USER (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("SELECT * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
