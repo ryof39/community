46dc9116d7b06a61db4f970442b251824c46816d
qianchenghui <qch11111@frsky-rc.com>
2021-04-09 22:04:10 +0800

    2021/4/9 P32 修复登陆问题

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -35,9 +36,9 @@ public class AuthorizeController {
     private UserService userService;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state,
-                           HttpServletResponse response){
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -48,7 +49,7 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
         System.out.println(githubUser.getName());//测试是否能够正确读取user.name等信息
         System.out.println(githubUser.getBio());
-        if (githubUser!=null && githubUser.getId()!=null){
+        if (githubUser != null && githubUser.getId() != null) {
             User user = new User();
             String token = UUID.randomUUID().toString();
             user.setToken(token);
@@ -56,16 +57,23 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setAvatarUrl(githubUser.getAvatarUrl());
             userService.createOrUpdate(user);
-            response.addCookie(new Cookie("token",token));
-//            //登陆成功，写Cookie和Session
-//
-//            request.getSession().setAttribute("user",githubUser); 不再写到session中了，因此Index里面没办法显示session.user.getname()
+            response.addCookie(new Cookie("token", token));
+
             return "redirect:/";
-        }else{
+        } else {
             //登陆失败，重新登陆
             return "redirect:/";
         }
 
-//        return "index";
+    }
+    @GetMapping("/logout")
+    public String  logout(HttpServletRequest request,
+                          HttpServletResponse response){
+        request.getSession().removeAttribute("user");
+        Cookie cookie = new Cookie("token",null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+
+        return "redirect:/";
     }
 }
