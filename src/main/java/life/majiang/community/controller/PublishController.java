963058e0f902ae1e0b4f587b5043acf587f535f6
qianchenghui <qch11111@frsky-rc.com>
2021-04-09 09:35:38 +0800

    2021/4/9 P22

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -32,36 +32,37 @@ public class PublishController {
             @RequestParam("description") String description,
             @RequestParam("tag") String tag,
             HttpServletRequest request,
-            Model model){
+            Model model) {
 
-        model.addAttribute("title",title);
-        model.addAttribute("description",description);
-        model.addAttribute("tag",tag);
-        if (title==null||title==""){
-            model.addAttribute("error","标题不能为空");
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+        if (title == null || title == "") {
+            model.addAttribute("error", "标题不能为空");
             return "publish";
         }
-        if (description==null||description==""){
-            model.addAttribute("error","问题补充不能为空");
+        if (description == null || description == "") {
+            model.addAttribute("error", "问题补充不能为空");
             return "publish";
         }
-        if (tag==null||tag==""){
-            model.addAttribute("error","标签不能为空");
+        if (tag == null || tag == "") {
+            model.addAttribute("error", "标签不能为空");
             return "publish";
         }
 
 
-        User user=null;
+        User user = null;
         Cookie[] cookies = request.getCookies();
-        if (cookies==null) return "index";              //自己加入的，判断cookies是否为null
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                user=userMapper.findByToken(token);
-                if (user!=null){
-                    request.getSession().setAttribute("user",user);
+        if (cookies != null && cookies.length != 0){
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
         }
         if (user==null){
