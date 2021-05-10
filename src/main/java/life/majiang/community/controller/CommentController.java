3e55b13fb5a7ece09fa081bfeec0bfce495151bc
qianchenghui <qch11111@frsky-rc.com>
2021-05-10 23:34:01 +0800

    2021/5/10 P44 1:02:00

--- src/main/java/life/majiang/community/controller/CommentController.java
+++ src/main/java/life/majiang/community/controller/CommentController.java
@@ -1,7 +1,9 @@
 package life.majiang.community.controller;
 
 import life.majiang.community.dto.CommentCreateDTO;
+import life.majiang.community.dto.CommentDTO;
 import life.majiang.community.dto.ResultDTO;
+import life.majiang.community.enums.CommentTypeEnum;
 import life.majiang.community.exception.CustomizeErrorCode;
 import life.majiang.community.model.Comment;
 import life.majiang.community.model.User;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -48,5 +51,13 @@ public class CommentController {
         objectObjectHashMap.put("message", "成功");
 
         return ResultDTO.okOf();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/comment/{id}", method = RequestMethod.GET)
+    public ResultDTO<List<CommentDTO>> comments(@PathVariable(name="id") Long id){
+        System.out.println(id);
+        List<CommentDTO> commentDTOS = commentService.listByTargetId(id, CommentTypeEnum.COMMENT);
+        return ResultDTO.okOf(commentDTOS);
     }
 }
--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,8 +1,14 @@
 
-
+/*
+提交回复
+*/
 function post(){
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
+    comment2Target(questionId,1,content);
+
+}
+function comment2Target(targetId,type,content){
     if (!content){
         alert("不能回复空内容");
         return;
@@ -12,9 +18,9 @@ function post(){
         url:"/comment",
         contentType:'application/json',
         data: JSON.stringify({
-            "parentId":questionId,
+            "parentId":targetId,
             "content":content,
-            "type":1
+            "type":type
         }),
         success:function(response){
             if(response.code==200){
@@ -35,6 +41,20 @@ function post(){
         },
         dataType:"json"
     });
-    console.log(questionId);
-    console.log(content);
+}
+function comment(e){
+    var commentId = e.getAttribute("data-id");
+    var content = $("#input-"+commentId).val();
+    comment2Target(commentId,2,content);
+}
+/*
+展开二级评论
+*/
+function collapseComments(e){
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-"+id);
+
+    // comments.addClass("in");
+    comments.toggleClass("in")
+
 }
