e38d2ee908b6d6fd3c7ae6db9b0f3f9993f291c1
qianchenghui <qch11111@frsky-rc.com>
2021-05-10 19:14:38 +0800

    2021/5/10 P41

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -17,7 +17,15 @@ function post(){
             if(response.code==200){
                 $("#comment_section").hide();
             }else{
-                alert(response.message);
+                if (response.code==2003){
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=bee9e0916ee74d7263eb&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable",true);
+                    }
+                }else{
+                    alert(response.message);
+                }
             }
             console.log(response)
         },
