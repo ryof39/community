e2e2b30fc35dc5f501443cd9fcb5c696ab64f603
qianchenghui <qch11111@frsky-rc.com>
2021-05-10 20:42:25 +0800

    2021/5/10 P44

--- src/main/java/life/majiang/community/controller/CommentController.java
+++ src/main/java/life/majiang/community/controller/CommentController.java
@@ -6,6 +6,7 @@ import life.majiang.community.exception.CustomizeErrorCode;
 import life.majiang.community.model.Comment;
 import life.majiang.community.model.User;
 import life.majiang.community.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +31,9 @@ public class CommentController {
         if (user == null) {
             ResultDTO resultDTO = ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
             return resultDTO;
+        }
+        if (commentCreateDTO==null || StringUtils.isBlank(commentCreateDTO.getContent())){
+            return ResultDTO.errorOf(CustomizeErrorCode.CONTENT_IS_EMPTY);
         }
         Comment comment = new Comment();
         comment.setParentId(commentCreateDTO.getParentId());
